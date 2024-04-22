9714cf2606958fdfb5db4c0ffdc0ad49333a2fac
Andreas Rath <andreas.rath@ondewo.com>
2024-04-22 14:08:11 +0200

    Fixed generation

--- public-api.d.ts
+++
@@ -1,3 +0,0 @@
-export * from './api/google/protobuf/empty_pb.d';
-export * from './api/ondewo/s2t/speech-to-text_pb.d';
-export * from './api/ondewo/s2t/speech-to-text_grpc_web_pb.d';
--- public-api.js
+++
@@ -1,3 +0,0 @@
-export * from './api/google/protobuf/empty_pb';
-export * from './api/ondewo/s2t/speech-to-text_pb';
-export * from './api/ondewo/s2t/speech-to-text_grpc_web_pb';
